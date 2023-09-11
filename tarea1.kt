ca685b49ab2e5d0248f627a512855faf2cb1d48a
Sebastian Estrada <sebastianestrada@Sebastians-MacBook-Air.local>
2023-09-10 22:31:17 -0600

    agregue tarea1

---
+++ tarea1.kt
@@ -0,0 +1,4 @@
+fun main() {
+    println("Hello UPIICSA")
+    println("Hola soy Sebastian")
+}
--- tarea1/src/jsMain/kotlin/Client.kt
+++
@@ -1,8 +0,0 @@
-import kotlinx.browser.document
-import react.create
-import react.dom.client.createRoot
-
-fun main() {
-    println("Hello UPIICSA")
-    println("Hola soy Sebastian")
-}
